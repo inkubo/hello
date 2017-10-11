cbe62ca0cc64b9fc1c5c086c1c02375799e65874
Carlos Pineda <clpineda@gmail.com>
2017-10-11 08:13:18 -0400

    second 3

--- src/ConsoleApplication1/Program.cs
+++ src/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello" 2");
+            Console.WriteLine("hello 2");
         }
     }
 }
